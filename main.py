e5fe1d08679e536de0f2edd63d257fcb46685b17
taldoflemis <coderflemis@gmail.com>
2023-03-25 07:39:00 -0300

    adicionar neighbors, usar stdin e adicionar edges

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 #!/bin/env python3
+import sys
 
 
 class Graph:
-    def __init__(self, filename):
+    def __init__(self):
         n = 0
         edges = []
-        with open(filename, "r") as f:
-            for i, line in enumerate(f):
-                if i == 2:
-                    n = int(line.split("=")[1])
-                elif i >= 4:
-                    edges.append([int(x) for x in line.split()])
-        self.n = n
+        for idx, line in enumerate(sys.stdin):
+            if idx == 2:
+                n = int(line.split("=")[1])
+            elif idx >= 4:
+                edges.append([int(x) for x in line.split()])
+        self.adj = [[0 for _ in range(n)] for _ in range(n)]
+        for edge in edges:
+            self.adj[edge[0] - 1][edge[1] - 1] = 1
+            self.adj[edge[1] - 1][edge[0] - 1] = 1
         self.edges = edges
-        self.adj = [[1] for _ in range(n)]
 
-    def output(self):
-        print(self.n)
-        for i in range(self.n):
+    def neighbors(self, v):
+        return [x for x in self.adj[v] if x != 0]
+
+    def usguri(self):
+        for i in range(self.adj.__len__()):
             print(self.adj[i])
 
 
 def main():
-    graph = Graph("./exemplos/instancias/0.in")
-    graph.output()
+    graph = Graph()
+    graph.usguri()
 
 
 if __name__ == "__main__":
