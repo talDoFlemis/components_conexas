1e728df516c2d18f0ae46516b1862a74a27ec3af
taldoflemis <coderflemis@gmail.com>
2023-03-25 19:21:01 -0300

    python eh uma merda

--- main.py
+++ main.py
@@ -4,8 +4,58 @@ import timeit
 from collections import deque
 
 
+def find(parents, a):
+    root = a
+
+    while parents[root] != root:
+        parents[root] = parents[parents[root]]
+        root = parents[root]
+    return root
+
+
+def union(parents, rank, a, b):
+    pa, pb = find(parents, a), find(parents, b)
+
+    if rank[pa] > rank[pb]:
+        parents[pb] = pa
+        rank[pa] += rank[pb]
+    elif rank[pb] > rank[pa]:
+        parents[pa] = pb
+        rank[pb] += rank[pa]
+    else:
+        parents[pb] = pa
+        rank[pa] += rank[pb]
+
+
 def should_be_fast_as_fk():
-    print("Hello World!")
+    parents = []
+    rank = []
+    path = []
+    n = 0
+    for idx, line in enumerate(sys.stdin):
+        if idx == 2:
+            n = int(line.split("=")[1])
+        if idx == 3:
+            parents = [i for i in range(n)]
+            path = [[] for _ in range(n)]
+            rank = [1] * n
+        elif idx >= 4:
+            a, b = [int(x) for x in line.split()]
+            union(parents, rank, a - 1, b - 1)
+
+    for idx, p in enumerate(parents):
+        if p == idx:
+            path[p].append(idx + 1)
+            for i in range(n):
+                if parents[i] == p and i != p:
+                    path[p].append(i + 1)
+
+    for p in path:
+        if p.__len__() != 0:
+            # sys.stdout.write(" ".join([str(x) for x in p]))
+            # sys.stdout.write("\n")
+            # sys.stdout.flush()
+            print(" ".join([str(x) for x in p]))
 
 
 class Graph:
@@ -46,19 +96,55 @@ class Graph:
                 i.sort()
                 print(" ".join([str(x + 1) for x in i]))
 
-    # def output(self):
-    #     for conn in self.componentes():
-    #         if conn.__len__() != 0:
-    #             conn.sort()
-    #             print(" ".join([str(x + 1) for x in conn]))
+
+class Graph2:
+    def __init__(self):
+        n = 0
+        self.adj = []
+        for idx, line in enumerate(sys.stdin):
+            if idx == 2:
+                n = int(line.split("=")[1])
+            if idx == 3:
+                self.adj = [[] for _ in range(n)]
+            elif idx >= 4:
+                (a, b) = [int(x) for x in line.split()]
+                self.adj[a - 1].append(b - 1)
+                self.adj[b - 1].append(a - 1)
+        self.n = n
+
+    def dfs(self, temp, v, vis):
+        vis[v] = True
+        temp.append(v)
+
+        for i in self.adj[v]:
+            if not vis[i]:
+                temp = self.dfs(temp, i, vis)
+        return temp
+
+    def output(self):
+        vis = []
+        conn = []
+        for _ in range(self.n):
+            vis.append(False)
+        for v in range(self.n):
+            if not vis[v]:
+                temp = []
+                conn.append(self.dfs(temp, v, vis))
+
+        for i in conn:
+            i.sort()
+            print(" ".join([str(x + 1) for x in i]))
 
 
 def main():
-    graph = Graph()
-    graph.output()
+    # g = Graph2()
+    # g.output()
+    should_be_fast_as_fk()
+    # graph = Graph()
+    # graph.output()
 
 
 if __name__ == "__main__":
-    main()
-    # time = timeit.timeit(main)
-    # print(time)
+    # main()
+    time = timeit.timeit(main)
+    print(time)
